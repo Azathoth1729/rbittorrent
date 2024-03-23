c33ac069769ad0a8431a2e07afb0fbeb3b0c420f
Azathoth1729 <aa1203528272@gmail.com>
2024-03-23 14:44:45 +0800

    checkpoint(2): decode bancoded int

---
+++ src/args.rs
@@ -0,0 +1,16 @@
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    Decode {
+        msg: String,
+    }
+}
--- src/value.rs
+++ src/value.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 /// Represents any valid Bencode value.
-#[derive(Clone)]
+#[warn(dead_code)]
 pub enum BenCode {
     Int(i64),
     String(String),
     Array(Vec<BenCode>),
-    Dict(HashMap<String, BenCode>)
+    Dict(HashMap<String, BenCode>),
 }
